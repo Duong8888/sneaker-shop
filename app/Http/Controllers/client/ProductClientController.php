d9cb115bdc7315e07470463747fe914bdf1e24a0
dtanhch116 <dtanhch116@gmail.com>
2023-07-27 15:48:32 +0700

    query img

--- app/Http/Controllers/client/ProductClientController.php
+++ app/Http/Controllers/client/ProductClientController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductClientController extends Controller
 {
     public function index(){
-        return view('client.product.index');
+        $data = Product::query()->with('images')->get();
+
+
+        return view('client.product.index',compact('data'));
     }
 }
--- resources/views/client/product/index.blade.php
+++ resources/views/client/product/index.blade.php
@@ -1,5 +1,7 @@
 @extends('client.templates.layout')
 @section('content')
+
+
     <!--product area start-->
     <section class="product_area mb-50">
         <div class="container">
