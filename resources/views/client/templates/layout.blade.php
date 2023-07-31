e37b1851ffa7e67d3efa4b165134bb2d5aaa6bdf
dtanhch116 <dtanhch116@gmail.com>
2023-07-31 21:38:18 +0700

    a

--- resources/views/client/templates/layout.blade.php
+++ resources/views/client/templates/layout.blade.php
@@ -474,24 +474,6 @@
 
 
 <!--slider area end-->
-@if(session()->has('myCart'))
-    @php
-        $myCartData = session('myCart');
-    @endphp
-
-    {{-- Hiển thị giá trị trong session --}}
-    @foreach($myCartData as $cartItem)
-{{--        @foreach($cartItem as $v)--}}
-{{--            <p>ProductId: {{ $v }}</p>--}}
-{{--        @endforeach--}}
-
-        <p>ColorId: hihi</p>
-{{--        <p>SizeId: {{ $cartItem['size_id'] }}</p>--}}
-{{--        <p>Quantity: {{ $cartItem['quantity'] }}</p>--}}
-{{--        <p>Quantity: {{ $cartItem['name'] }}</p>--}}
-        <hr>
-    @endforeach
-@endif
 
 
 @yield('content')
